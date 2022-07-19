9172737854a80f4bd8436dca897c20943c3b2f2e
kassiope91 <109542964+kassiope91@users.noreply.github.com>
2022-07-19 11:17:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 })
-.header{
- background-image: url('https://storge.pic2.me/w/560x350/934/5af9436344070.webp');
- background-attachment: fixed;
- background-size: cover;
- background-position: center;
-}
