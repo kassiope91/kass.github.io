468d76557e9d86cd0ae6cdb799c5a81d4628d161
kassiope91 <109542964+kassiope91@users.noreply.github.com>
2022-08-08 22:14:06 +0200

    Final project
    all changes

--- main.js
+++ main.js
@@ -5,9 +5,22 @@ $(document).ready(function(){
 $(".navbar a").click(function(){
 $("body,html").animate({
   scrollTop:$("#" + $(this).data('value')).offset().top
-},1000)
+},1000);
  })
 
-let min = 1;
-let max = 100;
-let random = min + Math.random() * (max - min);
+$("#btnGo").click(function() {
+    $("body,html").animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+})
+
+$("[data-original]").click(function() {
+    window.open($(this).data('original'), '_blank');
+})
+
+function getRandom() {
+    let min = +document.querySelector('#min').value;
+    let max = +document.querySelector('#max').value;
+    let rand = Math.floor(min + Math.random() * (max - min + 1));
+    document.querySelector('.random-result').innerText = rand;
+}
